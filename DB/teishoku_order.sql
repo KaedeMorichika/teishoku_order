379f2076d2fdbf6c1ee737d6af0ad23d1eb76c83
KaedeMorichika <>
2021-01-10 15:02:09 +0900

    sql 更新

--- DB/teishoku_order.sql
+++ DB/teishoku_order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: db
--- 生成日時: 2020 年 12 月 27 日 12:44
+-- 生成日時: 2021 年 1 月 10 日 05:56
 -- サーバのバージョン： 5.7.32-log
 -- PHP のバージョン: 7.4.13
 
@@ -20,6 +20,60 @@ SET time_zone = "+00:00";
 --
 -- データベース: `teishoku_order`
 --
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `info` varchar(255) NOT NULL,
+  `created` int(11) NOT NULL,
+  `updated` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `dish`
+--
+
+CREATE TABLE `dish` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `main_category_id` int(11) NOT NULL,
+  `info` varchar(255) NOT NULL,
+  `created` int(11) NOT NULL,
+  `updated` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `dish`
+--
+
+INSERT INTO `dish` (`id`, `name`, `price`, `main_category_id`, `info`, `created`, `updated`) VALUES
+(1, '唐揚げ定食', 720, 0, 'オーソドックスな唐揚げ定食。サラリーマンに大人気。選べるソースも嬉しいポイント。', 1609073787, 1609073787),
+(2, 'チキン南蛮定食', 840, 0, '唐揚げ定食から生まれた新メニュー。ガッツリいきたいあなたに。', 1609073991, 1609073991),
+(3, 'カレー定食', 550, 0, '老若男女に人気なカレーをお手頃価格で。', 1609074039, 1609074039),
+(4, '回鍋肉定食', 800, 0, '回鍋肉定食', 1609330315, 1609330315);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `relate_dish_category`
+--
+
+CREATE TABLE `relate_dish_category` (
+  `id` int(11) NOT NULL,
+  `dish_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `created` int(11) NOT NULL,
+  `updated` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -48,6 +102,24 @@ INSERT INTO `user` (`id`, `name`, `user_id`, `password`, `created`, `updated`) V
 --
 
 --
+-- テーブルのインデックス `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `dish`
+--
+ALTER TABLE `dish`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `relate_dish_category`
+--
+ALTER TABLE `relate_dish_category`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- テーブルのインデックス `user`
 --
 ALTER TABLE `user`
@@ -56,6 +128,24 @@ ALTER TABLE `user`
 --
 -- ダンプしたテーブルの AUTO_INCREMENT
 --
+
+--
+-- テーブルの AUTO_INCREMENT `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- テーブルの AUTO_INCREMENT `dish`
+--
+ALTER TABLE `dish`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- テーブルの AUTO_INCREMENT `relate_dish_category`
+--
+ALTER TABLE `relate_dish_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- テーブルの AUTO_INCREMENT `user`
